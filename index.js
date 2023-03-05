09234049f8ee456c3ec5e70a1f528afe5ab02887
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-05 16:11:25 -0500

    continues sketching out possibilities, looking for use of classes

--- index.js
+++ index.js
@@ -22,12 +22,43 @@ const questions = [
 ]
 
 const makeFile = function(answers) {
-  fs.writeFile(filename, answers, "utf8", (err) => err ? console.log(err) : console.log("SVG MADE"));
+  fs.writeFile(filename, answers, "utf8", (err) => err ? console.log(err) : console.log("Generated logo.svg" ));
+}
+
+const svgText = function(answers) {
+  if (answers.shape === "circle") {
+  return `
+    <svg version="1.1"
+    width="300" height="200"
+    xmlns="http://www.w3.org/2000/svg">
+    <circle cx="150" cy="100" r="${answers.radius}" fill="${answers.logocolor}" />
+    <text x="150" y="100" font-size="60" text-anchor="middle" fill="white">${answers.logotext}</text>
+    </svg>
+    `
+  } else if (answers.shape === "square") {
+    return `
+      <svg version="1.1"
+      width="300" height="200"
+      xmlns="http://www.w3.org/2000/svg">
+      <rect cx="150" cy="100" width="${answers.side}" height="${answers.side}" fill="${answers.logocolor}" />
+      <text x="150" y="100" font-size="60" text-anchor="middle" fill="white">${answers.logotext}</text>
+      </svg>
+      `
+  } else {
+    return `
+    <svg version="1.1"
+    width="300" height="200"
+    xmlns="http://www.w3.org/2000/svg">
+    <polygon points="150,0 0,200 300,200" fill="${answers.logocolor}" />
+    <text x="150" y="100" font-size="60" text-anchor="middle" fill="white">${answers.logotext}</text>
+    </svg>
+    `
+  }
 }
 
 const init = function () {
   prompt(questions)
-    .then((answers) => makeFile("newSVG.svg", answers))
+    .then((answers) => makeFile("logo.svg", svgText(answers)))
 }
 
 init();
