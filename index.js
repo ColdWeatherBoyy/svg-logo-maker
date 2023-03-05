b1dae4e4f52d5d3f12c3b32aa57d7eb8f549ed9b
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-05 16:44:47 -0500

    adds destructure, demonstrates solution (without tests yet). Still needs to add limitation to character adding and figure out purpose of classes

--- index.js
+++ index.js
@@ -26,29 +26,30 @@ const questions = [
   },
 ]
 
-const makeFile = function(answers) {
+const makeFile = function(filename, answers) {
   fs.writeFile(filename, answers, "utf8", (err) => err ? console.log(err) : console.log("Generated logo.svg" ));
 }
 
 const svgText = function(answers) {
-  if (answers.shape === "circle") {
+  const {shapecolor, textcolor, text, shape } = answers;
+  if (shape === "circle") {
   return `
     <svg version="1.1"
     width="300" height="200"
     xmlns="http://www.w3.org/2000/svg">
     <rect width="100%" height="100%" fill="white" />
-    <circle cx="150" cy="100" r="75" fill="${answers.shapecolor}" />
-    <text x="150" y="100" font-size="40" text-anchor="middle" dominant-baseline="central" fill="${answers.textcolor}">${answers.text}</text>
+    <circle cx="150" cy="100" r="75" fill="${shapecolor}" />
+    <text x="150" y="100" font-size="40" text-anchor="middle" dominant-baseline="central" fill="${textcolor}">${text}</text>
     </svg>
     `
-  } else if (answers.shape === "square") {
+  } else if (shape === "square") {
     return `
       <svg version="1.1"
       width="300" height="200"
       xmlns="http://www.w3.org/2000/svg">
       <rect width="100%" height="100%" fill="white" />
-      <rect x="75" y="25" width="150" height="150" fill="${answers.shapecolor}"/>
-      <text x="150" y="100" font-size="40" text-anchor="middle" dominant-baseline="central" fill="${answers.textcolor}">${answers.text}</text>
+      <rect x="75" y="25" width="150" height="150" fill="${shapecolor}"/>
+      <text x="150" y="100" font-size="40" text-anchor="middle" dominant-baseline="central" fill="${textcolor}">${text}</text>
       </svg>
       `
   } else {
@@ -56,8 +57,9 @@ const svgText = function(answers) {
     <svg version="1.1"
     width="300" height="200"
     xmlns="http://www.w3.org/2000/svg">
-    <polygon points="150,0 0,200 300,200" fill="${answers.shapecolor}" />
-    <text x="150" y="100" font-size="40" text-anchor="middle" dominant-baseline="central" fill="${textcolor}">${answers.text}</text>
+    <rect width="100%" height="100%" fill="white" />
+    <polygon points="150,20 40,180 260,180" fill="${shapecolor}" />
+    <text x="150" y="100" font-size="40" text-anchor="middle" dominant-baseline="central" fill="${textcolor}">${text}</text>
     </svg>
     `
   }
